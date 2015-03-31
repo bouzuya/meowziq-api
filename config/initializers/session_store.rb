34db93fb8077a4768a6eec3ae19c77ac2f23ee7e
bouzuya <m@bouzuya.net>
2015-03-31 15:54:12 +0900

    rename party-play to meowziq

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_party-play-api_session'
+Rails.application.config.session_store :cookie_store, key: '_meowziq-api_session'
